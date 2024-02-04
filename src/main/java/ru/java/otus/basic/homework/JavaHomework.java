2dadd8d8536c6a6fcfabd6046e68657e80d6cb1e
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-02-04 15:37:12 +0800

    Initial commit

---
+++ src/main/java/ru/java/otus/basic/homework/JavaHomework.java
@@ -0,0 +1,11 @@
+package ru.java.otus.basic.homework;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class JavaHomework {
+    public static void main(String[] args) {
